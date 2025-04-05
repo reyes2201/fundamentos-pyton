28974cf0e11f165af76ee5333eab3cf7d1d254e0
reyes2201 <reyesb2201@gmail.com>
2025-04-05 17:53:15 -0500

    primeros pasos de Python

---
+++ Mayor_de_tres_numero.py
@@ -0,0 +1,4 @@
+a, b, c= 5, 10, 3
+print("el mayor es:" , max (a, b, c)) #maximo de tres numeros 
+
+print("el menor es:" , min(a, b, c)) #minimo de tres numeros
---
+++ numero_par_impar.py
@@ -0,0 +1,5 @@
+num = int(input("Digite un numero:"))
+if num % 2 == 0:
+    print("numero es par.")
+else:
+    print("es impar.")
